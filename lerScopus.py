45f72e971d9fff7c4c0e4837076707504cf1259d
Cristiano Manhães de Oliveira <cristianomoliveira@gmail.com>
2019-11-13 02:30:21 -0300

    enviando graficos

--- lerScopus.py
+++ lerScopus.py
@@ -4,8 +4,8 @@ print("limite")
 print(csv.field_size_limit())
 csv.field_size_limit(100000000)
 
-arquivo = open('scopus.csv')
-corpus = open('corpus.txt', 'w')
+arquivo = open('teste(01-11)/scopus-01-11.csv')
+corpus = open('teste(01-11)/corpus-01-11.txt', 'w')
 
 linhas = csv.reader(arquivo)
 artigos = csv.DictReader(arquivo)
